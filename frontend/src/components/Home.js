e76d9b91e408f4774e21838ab57d0b336c614df2
Ujjal <khadkauzal@gmail.com>
2022-05-19 16:48:02 +0545

    completed-user-functions

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,21 +1,29 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import EmployeeList from './EmployeeList'
+import PersonalReports from './PersonalReports'
 
 const Home = () => {
+  // const dispatch = useDispatch()
+
+  // const userDetails = useSelector((state) => state.userDetails)
+  // const { loading, error, user } = userDetails
+
   return (
-    <>
+    <div style={{ marginTop: '100px' }}>
+      {/* {user.role === '$$MY_&&nAme_..AdmiNN,_saN@&**' ? (
+        <EmployeeList />
+      ) : (
+        <PersonalReports />
+      )} */}
       <div>Home</div>
       <div>Home</div>
       <div>Home</div>
       <div>Home</div>
       <div>Home</div>
       <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-      <div>Home</div>
-    </>
+    </div>
   )
 }
 
